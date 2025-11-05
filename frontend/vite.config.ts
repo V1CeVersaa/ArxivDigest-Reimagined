379e7453b550a6e96bccd305ad779298a78d9561
V1CeVersaa <285424882@qq.com>
2025-11-05 16:56:37 +0800

    chore: fix base_url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.BASE_URL || (process.env.GITHUB_ACTIONS ? "/ArxivDigest-Reimagined/" : "/"),
+    base: "/",
     plugins: [vue(), vueJsx(), vueDevTools()],
     resolve: {
         alias: {
